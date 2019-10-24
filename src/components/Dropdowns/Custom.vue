90a9d40068e64d447e7f0db5373cb8efa9a13b78
Lewis Cains <l.cains@tshirtandsons.co.uk>
2019-10-24 13:05:42 +0100

    dropdown done

--- src/components/Dropdowns/Custom.vue
+++ src/components/Dropdowns/Custom.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="custom-dropdown-container mx-auto mt-10">
-    <div class="p-1">{{ selectedOption.name || 'Please choose an option' }}</div>
-    <div v-if="showOptions || true">
+  <div v-click-outside="reset" class="custom-dropdown-container mx-auto mt-10 p-4">
+    <div class="p-1" @click="showOptions = true">{{ selectedOption.name || 'Please choose an option' }}</div>
+    <div v-if="showOptions">
       <input v-model="filter" class="search-filter">
       <ul>
-        <li :class="{'bg-green text-white': selectedOption.name === option.name}" @click="selectOption(option)" :key="`dropdown-option-${option.id}`" v-for="option in dropdownOptions">
+        <li :class="{'bg-green text-white': selectedOption.name === option.name}" @click="selectOption(option)" :key="`dropdown-option-${option.id}`" v-for="option in filteredOptions">
           {{ option.name }}
         </li>
       </ul>
@@ -13,11 +13,30 @@
 </template>
 
 <script>
+import ClickOutside from 'vue-click-outside';
+
 export default {
   name: 'CustomDropdown',
+  computed: {
+    filteredOptions() {
+      return this.dropdownOptions.filter(o => {
+        if (this.filter === '') return true;
+        return o.name.toLowerCase().indexOf(this.filter.toLowerCase()) !== -1;
+      });
+    },
+  },
+  directives: {
+    ClickOutside,
+  },
   methods: {
+    reset() {
+      // this.selectedOption = {};
+      this.filter = '';
+      this.showOptions = false;
+    },
     selectOption(option) {
       this.selectedOption = option;
+      this.reset();
     },
   },
   data() {
@@ -50,7 +69,6 @@ export default {
   max-width:400px;
   border: 1px solid #ccc;
   color: #212121;
-  background:pink;
   ul{
     list-style-type:none;
     margin:0;
@@ -64,7 +82,8 @@ export default {
     }
   }
   .search-filter{
-    @apply w-full p-1;
+    width: 97%;
+    @apply my-1 p-1;
   }
 }
 </style>
